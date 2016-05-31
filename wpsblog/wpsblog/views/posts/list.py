53fd9c562220acd52cccd542042404af1cc4b303
Philip Nam <philipnam101@gmail.com>
2016-06-01 00:46:15 +0900

    update list.py: added search feature. Can only search for full title

--- wpsblog/wpsblog/views/posts/list.py
+++ wpsblog/wpsblog/views/posts/list.py
@@ -4,10 +4,16 @@ from wpsblog.models import Post
 
 
 def list(request):
+    search = request.GET.get('search')
+    post_list = Post.objects.all()
+    if search:
+        post_list = Post.objects.filter(title=search)
+
     return render(
             request,
             'posts/list.html',
             {
                 'site_name': 'Blog List',
-                'post_list': Post.objects.all()}
+                'post_list': post_list,
+            }
     )
