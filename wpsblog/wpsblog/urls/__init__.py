625a663d026796732d4488f30f6d6b11c33e1959
Philip Nam <philipnam101@gmail.com>
2016-05-31 20:47:56 +0900

    created home page

--- wpsblog/wpsblog/urls/__init__.py
+++ wpsblog/wpsblog/urls/__init__.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from wpsblog.views import *
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home, name='home'),
 ]
