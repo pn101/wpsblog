f3d349a400a49305eec847fd90a3626efb7d992e
Philip Nam <philipnam101@gmail.com>
2016-06-01 15:00:23 +0900

    update naver_posts_list.py: added search function

--- wpsblog/wpsblog/views/naver_posts/naver_posts_list.py
+++ wpsblog/wpsblog/views/naver_posts/naver_posts_list.py
@@ -4,10 +4,16 @@ from wpsblog.models import NaverPost
 
 
 def naver_posts_list(request):
+    keyword = request.GET.get('keyword')
+    naver_post_list = NaverPost.objects.all()
+    if keyword:
+        naver_post_list = [
+            post for post in naver_post_list if keyword in post.title
+        ]
     return render(
             request,
             'naver_posts/naver_posts_list.html',
             {
-                'naver_posts': NaverPost.objects.all()
+                'naver_posts': naver_post_list,
             }
     )
