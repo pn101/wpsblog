5c999b0638d65d887af3fc1c5a7be2e201d40820
Philip Nam <philipnam101@gmail.com>
2016-06-01 14:21:33 +0900

    update crawl_naver.py: PEP8 guideline amendments

--- wpsblog/wpsblog/management/commands/crawl_naver.py
+++ wpsblog/wpsblog/management/commands/crawl_naver.py
@@ -1,4 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
 from django.core.management.base import BaseCommand
+from wpsblog.models import NaverPost
 
 
 class Command(BaseCommand):
@@ -12,3 +16,30 @@ class Command(BaseCommand):
         self.stdout.write('Crawling {query} blog from Naver.'.format(
             query=query,
             ))
+
+        url = 'https://search.naver.com/search.naver?where=post&sm=tab_jum&ie=utf8&query={query}'.format(
+                query=query,
+        )
+
+        response = requests.get(url)
+        dom = BeautifulSoup(response.text, 'html.parser')
+
+        post_elements = dom.select('.sh_blog_top')
+
+        for post_element in post_elements:
+            title_element = post_element.select_one('.sh_blog_title')
+            title = title_element.text
+            url = title_element.get('href')
+
+            content_element = post_element.select_one('.sh_blog_passage')
+            content = content_element.text
+
+            image_element = post_element.select_one('.sh_blog_thumbnail')
+            image = image_element.get('src')
+
+            NaverPost.objects.create(
+                    title=title,
+                    original_url=url,
+                    content=content,
+                    thumbnail_image_url=image,
+            )
