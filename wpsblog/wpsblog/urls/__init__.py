f8c979fc5639397416470d88cca8b28c1933bd5c
Philip Nam <philipnam101@gmail.com>
2016-06-01 14:21:58 +0900

    update urls __init__.py

--- wpsblog/wpsblog/urls/__init__.py
+++ wpsblog/wpsblog/urls/__init__.py
@@ -11,4 +11,6 @@ urlpatterns = [
     url(r'^policy/', include('wpsblog.urls.policy', namespace='policy')),
     url(r'^posts/$', list, name='post-list'),
     url(r'^posts/(?P<post_id>\d+)/$', detail, name='post-detail'),
+    url(r'^naver/posts/$', naver_posts_list, name='naver_posts_list'),
+
 ]
