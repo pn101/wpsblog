a5ab5d50cdfd8ee1bf0870890bed95046939b5ee
Philip Nam <philipnam101@gmail.com>
2016-06-01 17:13:50 +0900

    init create and new views

---
+++ wpsblog/wpsblog/views/posts/new.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def new(request):
+    return render(
+            request,
+            'posts/new.html',
+            {
+                'site_name': 'New Post',
+            }
+    )
