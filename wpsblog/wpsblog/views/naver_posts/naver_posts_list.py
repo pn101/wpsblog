268e5b546fa8c3596f03440653530805b01738fa
Philip Nam <philipnam101@gmail.com>
2016-06-01 15:26:03 +0900

    update naver_posts_list.py: added search by keyword and query feature

--- wpsblog/wpsblog/views/naver_posts/naver_posts_list.py
+++ wpsblog/wpsblog/views/naver_posts/naver_posts_list.py
@@ -5,15 +5,21 @@ from wpsblog.models import NaverPost
 
 def naver_posts_list(request):
     keyword = request.GET.get('keyword')
+    query = request.GET.get('query')
     naver_post_list = NaverPost.objects.all()
+
     if keyword:
-        naver_post_list = [
-            post for post in naver_post_list if keyword in post.title
-        ]
+        naver_post_list = naver_post_list.filter(keyword=keyword)
+
+    if query:
+        naver_post_list = naver_post_list.filter(title__contains=query)
+
     return render(
             request,
             'naver_posts/naver_posts_list.html',
             {
+                'keyword': keyword,
+                'query': query,
                 'naver_posts': naver_post_list,
             }
     )
