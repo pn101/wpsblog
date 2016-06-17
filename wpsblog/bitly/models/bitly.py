c591c44eff3dab7661f854b662889122c975f354
pn101 <philipnam101@gmail.com>
2016-06-17 14:26:59 +0900

    init BitLink model

---
+++ wpsblog/bitly/models/bitly.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+
+class BitLink(models.Model):
+
+    user = models.ForeignKey(User)
+
+    original_url = models.URLField()
+
+    shorten_hash = models.CharField(
+            max_length=6,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return original_url
+
+    def get_absolute_url(self):
+        pass
