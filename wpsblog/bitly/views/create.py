3448a941e35f5ed80ff9b7d6589d4b9ac56b8827
pn101 <philipnam101@gmail.com>
2016-06-17 14:48:08 +0900

    init create.html: and amend associated urls and templates to enable create function

--- wpsblog/bitly/views/create.py
+++ wpsblog/bitly/views/create.py
@@ -7,9 +7,15 @@ from .base import BitLinkBaseView
 class BitLinkCreateView(BitLinkBaseView, LoginRequiredMixin, CreateView):
     template_name = 'create.html'
     fields = [
-        'content',
+        'original_url',
     ]
+    success_url = '/mypage/'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(BitLinkCreateView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(BitLinkCreateView, self).get_context_data(**kwargs)
+        context['site_name'] = 'Create BitLink'
+        return context
